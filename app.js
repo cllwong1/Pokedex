09d6feafc269958ec01a53f776aa996f0d34d080
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2020-09-09 18:27:26 +0800

    added sort by number ascending and descending order

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
-  console.log('hello')
+  console.log('linked')
 
+/*********************************Call the API using AJAX************************************************************/
   $.ajax({
     url: 'https://pokeapi.co/api/v2/pokemon?limit=20',
     error: function(){
@@ -8,12 +9,16 @@ document.addEventListener('DOMContentLoaded', function(){
     },
     success: function(data){
       data.results.forEach(function(item){
+
+        //Second call to API using AJAX
         $.ajax({
           url: item.url,
           error: function(){
             alert('API call to second pokemon endpoint failed')
           },
           success: function(data){
+
+            //Extract the data using the function
             extract_data(data)
           }
         })
@@ -23,32 +28,39 @@ document.addEventListener('DOMContentLoaded', function(){
   
 
 
-function extract_data(data){
-  console.log(data)
-  let div_row = document.querySelector('#list')
-  let div = document.createElement('div')
-  // div.setAttribute('id',data.id)
-  div.setAttribute('class','col-md-2half')
-  let order_number = "" + data.id
-  let format = "000"
-  let number = format.substring(0, format.length - order_number.length) + order_number
-  
-  
-  if(data.types.length === 1){
-    div.innerHTML = `
-      <span class="pokemon-id">${number}</span>
-      <div class="image-wrapper">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg" class="pokemon_image"><br>
-      </div>
-      <span class="pokemon-name">${data.name}</span>
-      <div class="text-box">
-        <span class=${data.types[0].type.name}>${data.types[0].type.name}</span>
-      </div>
-      `
+
+  /***************************Function to extract the API files**************************************************************/
+
+  function extract_data(data){
+
+    input_data(data)
+    sortList("Sort By")
+
+    let dropdownlist = document.getElementById('dropdown_list')
+    dropdownlist.addEventListener('change',function(e){
+      sortList(e.target.value)
+    })
+
+    // sortList()
+
   }
 
-  else if (data.types.length === 2){
-    div.innerHTML = `
+
+
+
+  /***************************Function to input the data into HTML FILE**************************************************************/
+  function input_data(data){
+
+    let div_row = document.querySelector('#list')
+    let div = document.createElement('div')
+    div.setAttribute('class','col-md-2half')
+    let order_number = "" + data.id
+    let format = "000"
+    let number = format.substring(0, format.length - order_number.length) + order_number
+    
+    
+    if(data.types.length === 1){
+      div.innerHTML = `
         <span class="pokemon-id">${number}</span>
         <div class="image-wrapper">
           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg" class="pokemon_image"><br>
@@ -57,75 +69,94 @@ function extract_data(data){
         <div class="text-box">
           <span class=${data.types[0].type.name}>${data.types[0].type.name}</span>
         </div>
-        <div class="text-box">
-          <span class=${data.types[1].type.name}>${data.types[1].type.name}</span>
-        </div>
         `
-  }
-
-  div_row.append(div)
-  sortList()
-}
-
-function sortList() {
-  let list = document.getElementById("list");
-  let switching = true;
-
-  while (switching) {
-    switching = false
-    let b = list.getElementsByClassName("col-md-2half");
-    for (let i=0; i < (b.length - 1); i++) {
-
-      if (Number(b[i].childNodes[1].textContent) > Number(b[i + 1].childNodes[1].textContent)) {
-          b[i].parentNode.insertBefore(b[i + 1], b[i]);
-          switching = true;
-      }
-
     }
+
+    else if (data.types.length === 2){
+      div.innerHTML = `
+          <span class="pokemon-id">${number}</span>
+          <div class="image-wrapper">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg" class="pokemon_image"><br>
+          </div>
+          <span class="pokemon-name">${data.name}</span>
+          <div class="text-box">
+            <span class=${data.types[0].type.name}>${data.types[0].type.name}</span>
+          </div>
+          <div class="text-box">
+            <span class=${data.types[1].type.name}>${data.types[1].type.name}</span>
+          </div>
+          `
+    }
+
+    div_row.append(div)
+
   }
-}
 
 
+
+
+  /***************************Function to Sort the List**************************************************************/
+  // function sortList() {
+  //   let list = document.getElementById("list");
+  //   let switching = true;
+
+  //   while (switching) {
+  //     switching = false
+  //     let b = list.getElementsByClassName("col-md-2half");
+  //     for (let i=0; i < (b.length - 1); i++) {
+
+  //       if (Number(b[i].childNodes[1].textContent) > Number(b[i + 1].childNodes[1].textContent)) {
+  //           b[i].parentNode.insertBefore(b[i + 1], b[i]);
+  //           switching = true;
+  //       }
+
+  //     }
+  //   }
+  // }
+
+  function sortList(dropdown_value) {
+
+  
+
+    let list = document.getElementById("list");
+    let switching = true;
+      while (switching) {
+        switching = false
+        let b = list.getElementsByClassName("col-md-2half");
+        for (let i=0; i < (b.length - 1); i++) {
+
+          if (dropdown_value === "Sort By" || dropdown_value === "opt 1"){
+            if (Number(b[i].childNodes[1].textContent) > Number(b[i + 1].childNodes[1].textContent)) {
+                b[i].parentNode.insertBefore(b[i + 1], b[i]);
+                switching = true;
+            }
+          }
+
+          if (dropdown_value === "opt 2"){
+            if (Number(b[i].childNodes[1].textContent) < Number(b[i + 1].childNodes[1].textContent)) {
+                b[i].parentNode.insertBefore(b[i + 1], b[i]);
+                switching = true;
+            }
+          }
+
+
+
+        }
+      }
+    
+  
+
+  }
+
+
+
+
+  // let dropdownlist = document.getElementById('dropdown_list')
+
+  // dropdownlist.addEventListener('change',function(e){
+  //   console.log(e.target.value)
+  // })
 
 
 
 })
-
-
-// function sortList() {
-//   var list, i, switching, b, shouldSwitch;
-//   list = document.getElementById("id01");
-//   switching = true;
-//   /* Make a loop that will continue until
-//   no switching has been done: */
-//   while (switching) {
-//     // start by saying: no switching is done:
-//     switching = false;
-//     b = list.getElementsByTagName("LI");
-//     // Loop through all list-items:
-//     for (i = 0; i < (b.length - 1); i++) {
-//       // start by saying there should be no switching:
-//       shouldSwitch = false;
-//       /* check if the next item should
-//       switch place with the current item: */
-      
-//       if (Number(b[i].innerHTML) > Number(b[i + 1].innerHTML)) {
-//         /* if next item is numerically
-//         lower than current item, mark as a switch
-//         and break the loop: */
-//         shouldSwitch = true;
-//         break;
-//       }
-//     }
-//     if (shouldSwitch) {
-//       /* If a switch has been marked, make the switch
-//       and mark the switch as done: */
-//       b[i].parentNode.insertBefore(b[i + 1], b[i]);
-//       switching = true;
-//     }
-//   }
-// }
-
-var str = "" + 1
-var pad = "0000"
-var ans = pad.substring(0, pad.length - str.length) + str
