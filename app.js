16f4b591ef9ea405e86ec91c86be89993ad68092
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2020-09-09 12:17:57 +0800

    JS DOM to insert all the pokemon completed

--- app.js
+++ app.js
@@ -40,41 +40,68 @@ document.addEventListener('DOMContentLoaded', function(){
 // }
 
 function extract_data(data){
+  console.log(data)
   let div_row = document.querySelector('#list')
   let div = document.createElement('div')
   // div.setAttribute('id',data.id)
-  div.setAttribute('class','col')
-  let order_number = ""+data.id
-  console.log(order_number.length)
-  let pad = "000"
-  let number = pad.substring(0, pad.length - order_number.length) + order_number
-  div.innerHTML = `
-  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg" class="pokemon_image"><br>
-  <span>${number}</span><br>
-  <span>${data.name}</span><br>`
+  div.setAttribute('class','col-md-2half')
+  let order_number = "" + data.id
+  let format = "000"
+  let number = format.substring(0, format.length - order_number.length) + order_number
+  
+  
+  if(data.types.length === 1){
+    div.innerHTML = `
+      <span class="pokemon-id">${number}</span>
+      <div class="image-wrapper">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg" class="pokemon_image"><br>
+      </div>
+      <span class="pokemon-name">${data.name}</span><br>
+      <div class="text-box">
+        <span class=${data.types[0].type.name}>${data.types[0].type.name}</span>
+      </div>
+      `
+  }
+
+  else if (data.types.length === 2){
+    div.innerHTML = `
+        <span class="pokemon-id">${number}</span>
+        <div class="image-wrapper">
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg" class="pokemon_image"><br>
+        </div>
+        <span class="pokemon-name">${data.name}</span><br>
+        <div class="text-box">
+          <span class=${data.types[0].type.name}>${data.types[0].type.name}</span>
+        </div>
+        <div class="text-box">
+          <span class=${data.types[1].type.name}>${data.types[1].type.name}</span>
+        </div>
+        `
+  }
+
   div_row.append(div)
   sortList()
 }
 
 function sortList() {
-  var list, i, switching, b, shouldSwitch;
-  list = document.getElementById("list");
-  switching = true;
+  let list = document.getElementById("list");
+  let switching = true;
   /* Make a loop that will continue until
   no switching has been done: */
   while (switching) {
     // start by saying: no switching is done:
     switching = false;
-    b = list.getElementsByClassName("col");
-    
+    let shouldSwitch = false;
+    let b = list.getElementsByClassName("col-md-2half");
+    let i = 0
     // Loop through all list-items:
-    for (i = 0; i < (b.length - 1); i++) {
+    for (i; i < (b.length - 1); i++) {
       // start by saying there should be no switching:
-      shouldSwitch = false;
+      
       /* check if the next item should
       switch place with the current item: */
       // console.log(b)
-      if (Number(b[i].childNodes[4].textContent) > Number(b[i + 1].childNodes[4].textContent)) {
+      if (Number(b[i].childNodes[1].textContent) > Number(b[i + 1].childNodes[1].textContent)) {
         /* if next item is numerically
         lower than current item, mark as a switch
         and break the loop: */
